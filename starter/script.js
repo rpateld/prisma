28f75814718f031136d097ecf8bcfc52b1cf42a9
rpateld <rameshbhai.patel2@toronto.ca>
2020-04-04 15:20:25 -0400

    done

--- starter/script.js
+++ starter/script.js
@@ -4,7 +4,26 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
-  // ... you will write your Prisma Client queries here
+
+  const post = await prisma.post.update({
+    where: { id: 2 },
+    data: { published: true }
+  })
+
+  // const post = await prisma.post.create({
+  //   data: {
+  //     title: "Prisma make database easy",
+  //     author: {
+  //       connect: { email: "sarah@prisma.io" }
+  //     }
+  //   }
+  // })
+  const allUsers = await prisma.user.findMany({
+    include: { posts: true }
+  })
+  // console.log(allUsers)
+  console.dir(allUsers, { depth: null })
+
 }
 
 main()
